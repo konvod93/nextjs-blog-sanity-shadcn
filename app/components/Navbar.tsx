521aa0a77c163c85adfb8ff8de457fa830650391
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-02-11 16:40:25 +0200

    created Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+            <div>
+                <h1>child-1</h1>
+            </div>
+            <div>
+                <h1>child-2</h1>
+            </div>
+        </nav>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>hello from the index page</h1>
     </div>
   );
