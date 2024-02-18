689c322dda37616284e60431658c6cdc461af774
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-02-18 18:45:47 +0200

    cleaned code from logs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -23,8 +23,7 @@ export default async function BlogArticle({
   params: { slug: string };
 }) {
   const data: fullBlog = await getData(params.slug);
-  console.log(data);
-
+  
   return (
     <div className="mt-8">
       <h1>
